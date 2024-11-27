c23ce6d7d976376298940383e3dd3b46ff4ff8cb
Anonymous <anonymous@example.com>
2024-11-27 21:35:15 +0800

    Update entrypoint script to avoid reinstalling on every container startup

--- .docker-entrypoint.sh
+++ .docker-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Path to flag file that indicates this container has been initialized
+INIT_FLAG_FILE="/.container_initialized"
+
+if [ -f "$INIT_FLAG_FILE" ]; then
+    echo "Container already initialized, skipping container initialization..."
+    exec "$@"
+    exit 0
+fi
+
+
+echo "First time container startup - initializating container..."
+
 # Check if Grena-verifier directory doesn't exist or is empty
 if [ ! -d "/app/Grena-verifier" ] || [ -z "$(ls -A /app/Grena-verifier 2>/dev/null)" ]; then
     echo "Cloning Grena-verifier repository..."
@@ -17,6 +29,10 @@ chmod +x install.sh
 
 # Download all the neural-network models
 bash download_models.sh
+
+# Create flag file to indicate that this container has been initialized
+echo "This file is to indicate that this Docker container has already been initialized." > "$INIT_FLAG_FILE"
+echo "Initialization complete"
 
 # Execute the provided command
 exec "$@"
