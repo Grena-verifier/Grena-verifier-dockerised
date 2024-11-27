75f7d65ab45e1050eaefb2b9ded74703a379fe79
Anonymous <anonymous@example.com>
2024-11-27 22:17:22 +0800

    Make entrypoint script more robust

--- .docker-entrypoint.sh
+++ .docker-entrypoint.sh
@@ -3,14 +3,17 @@
 # Path to flag file that indicates this container has been initialized
 INIT_FLAG_FILE="/.container_initialized"
 
-if [ -f "$INIT_FLAG_FILE" ]; then
+# If initialization flag file and repo exists, skip initialisation
+if [ -f "$INIT_FLAG_FILE" ] \
+    && ([ -d "/app/Grena-verifier" ] && [ ! -z "$(ls -A /app/Grena-verifier 2>/dev/null)" ])
+then
     echo "Container already initialized, skipping container initialization..."
     exec "$@"
     exit 0
 fi
 
 
-echo "First time container startup - initializating container..."
+echo "Initializating container..."
 
 # Check if Grena-verifier directory doesn't exist or is empty
 if [ ! -d "/app/Grena-verifier" ] || [ -z "$(ls -A /app/Grena-verifier 2>/dev/null)" ]; then
@@ -18,6 +21,9 @@ if [ ! -d "/app/Grena-verifier" ] || [ -z "$(ls -A /app/Grena-verifier 2>/dev/nu
     git clone https://github.com/Grena-verifier/Grena-verifier.git /app/Grena-verifier
     cd /app/Grena-verifier
     git submodule update --init --recursive
+
+    # Download all the neural-network models
+    bash download_models.sh
 fi
 
 cd /app/Grena-verifier
@@ -26,9 +32,6 @@ cd /app/Grena-verifier
 echo "Running Grena-verifier installation script..."
 chmod +x install.sh
 ./install.sh --use-cuda
-
-# Download all the neural-network models
-bash download_models.sh
 
 # Create flag file to indicate that this container has been initialized
 echo "This file is to indicate that this Docker container has already been initialized." > "$INIT_FLAG_FILE"
